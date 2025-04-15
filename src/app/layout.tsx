1e5e9619ef1adee3ca1160be25bf1a9868ddf029
GNO-SPS <atharva.mehendale@gmail.com>
2025-04-15 20:15:00 +0100

    Fix: add type to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Southpaw Space',
   description: 'Strategy, guides and gear for southpaw fighters.',
 };
