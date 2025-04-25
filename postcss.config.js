c7be525f8075e5df36be5005539955a2fbe89e4b
GNO-SPS <atharva.mehendale@gmail.com>
2025-04-25 15:13:57 +0100

    Fix Tailwind v4 import in globals.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 /** @type {import('postcss').Config} */
-module.exports = {
+// postcss.config.js
+export default {
   plugins: {
+    // you can remove postcss-import & autoprefixer entirely
+    // unless you really need them elsewhere
     '@tailwindcss/postcss': {},
-    autoprefixer: {},
   },
 }
