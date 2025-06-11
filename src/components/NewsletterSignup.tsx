9c71d97c01600a54dbf8b464cbfa373d8090c7d3
GNO-SPS <atharva.mehendale@gmail.com>
2025-06-12 00:09:19 +0100

    Use next/script for Kit embed to avoid duplicate forms

--- src/components/NewsletterSignup.tsx
+++ src/components/NewsletterSignup.tsx
@@ -1,22 +1,24 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function NewsletterSignup() {
-  useEffect(() => {
-    const existingScript = document.querySelector("script[data-uid='4482a7b1c7']");
-    if (!existingScript) {
-      const script = document.createElement("script");
-      script.async = true;
-      script.setAttribute("data-uid", "4482a7b1c7");
-      script.src = "https://southpaw-space.kit.com/4482a7b1c7/index.js";
-      document.body.appendChild(script);
-    }
-  }, []);
+  const uid = "4482a7b1c7";
+  const src = `https://southpaw-space.kit.com/${uid}/index.js`;
 
   return (
     <div className="w-full max-w-xl mx-auto my-10 px-4">
-      <div data-uid="4482a7b1c7"></div>
+      {/* This is where Kit will mount the form */}
+      <div data-uid={uid} />
+      
+      {/* Load the Kit embed script only when this component is rendered */}
+      <Script
+        src={src}
+        data-uid={uid}
+        strategy="afterInteractive"
+        onError={(e) => console.error("Kit embed failed", e)}
+      />
     </div>
   );
 }
+
